ce5beee5ecc7bdd9e50334369e3c50185de1608e
cbax-mora <cbax0619@gmail.com>
2022-07-19 06:56:51 -0500

    esta es la clase 2 de javascript

---
+++ clase-1/script/app.js
@@ -0,0 +1,32 @@
+let nombre = prompt("ingrese su nombre");
+let apellido = prompt("ingrese apellido");
+console.log(nombre+" "+apellido);
+//este es el ejemplo de constante
+const email="cbax0619@gmail.com";
+console.log(`el correo es: ${email}`);
+//este es el ejemplo de undefined
+let correo;
+console.log(`el correo es: ${correo}`);
+
+//mumuki ejercicios
+
+//ejercicio # 1
+// let nom1 = prompt("¿Cuál es tu nombre?");
+// let firtsname = prompt("¿Cuál es tu apellido?");
+// alert(`hola ${nom1} ${firtsname}, le damnos la bienvenida`);
+
+//ejercicio # 2
+// let gusto1 = prompt("¿ingresa primer gusto?");
+// let gusto2 = prompt("¿ingresa segundo gusto?");
+// let gusto3 = prompt("¿ingresa tercer gusto?");
+// alert(`Aquí tiene su helado de ${gusto1} ${gusto2} ${gusto3}`);
+
+//ejercicio # 3
+
+// let nombre = prompt("¿Cuál es tu nombre?");
+// let apellido = prompt("¿Cuál es tu apellido?");
+// let edad = prompt("¿Cuál es tu edad?");
+// let nacionalidad = prompt("¿Cuál es tu nacionalidad?");
+// let identificacion = prompt("¿Cuál es tu número de identificación personal?");
+// alert(`hola ${nombre} ${apellido} ${edad} ${nacionalidad}, número de identificación personal:${identificacion}`);
+
--- script/app.js
+++
@@ -1,9 +0,0 @@
-let nombre = prompt("ingrese su nombre");
-let apellido = prompt("ingrese apellido");
-console.log(nombre+" "+apellido);
-//este es el ejemplo de constante
-const email="cbax0619@gmail.com";
-console.log(`el correo es: ${email}`);
-//este es el ejemplo de undefined
-let correo;
-console.log(`el correo es: ${correo}`);

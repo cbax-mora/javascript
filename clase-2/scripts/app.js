bd97711b190a59f0dbf8aa5a2ca0fe37b4d7ca70
cbax-mora <cbax0619@gmail.com>
2022-07-19 09:11:36 -0500

    clase 2

---
+++ clase-2/scripts/app.js
@@ -0,0 +1,14 @@
+alert('hola mora')
+
+let firstname= 'sebastian';
+let lastname= 'mora';
+let gender= 'female';
+//let age = 26;
+
+if(gender=="male"){
+    let age=5;
+}else{
+    console.log(age)
+}
+
+console.log(`bienvenido ${firstname} ${lastname} su genero es: ${gender} y tiene ${age} años`);
---
+++ clase-2/scripts/index.js
@@ -0,0 +1,4 @@
+console.log(age)
+console.log(nombre)
+
+var nombre= 'carlos'

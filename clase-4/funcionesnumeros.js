4c34bc24c3f91e48d70312c3f51af0c74d35ed9b
cbax-mora <cbax0619@gmail.com>
2022-07-22 09:57:05 -0500

    funciones nativas para numeros

---
+++ clase-4/funcionesnumeros.js
@@ -0,0 +1,19 @@
+console.log(Math.E)//devuelve la constante de euler
+console.log(Math.PI)
+
+console.log(Math.pow(2,2))
+console.log(2**3)
+
+let numero1=30;
+let numero2=82;
+
+console.log(Math.min(numero1,numero2))
+console.log(Math.max(numero1,numero2))
+
+const numeroPI = 3.1416
+
+console.log(Math.round(numeroPI))
+console.log(Math.ceil(numeroPI))//devuelve valor entero mas pequeño redondeado hacia arriba 
+console.log(math.floor(numeroPI))//devuelve valor entero mas grande redondeado hacia arriba 
+
+//console.log(Math.trunc(Math.random()*(numero2-numero1)+numero1))

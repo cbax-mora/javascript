607151f56af578c4ab006b6c460feb3f86b0403f
cbax-mora <cbax0619@gmail.com>
2022-08-04 12:36:06 -0500

    clase 10

---
+++ clase-10/scripts/app.js
@@ -0,0 +1,6 @@
+let todosTitulos = document.getElementsByTagName('h1')
+
+console.log(todosTitulos)
+
+let todosSubtitulos = document.getElementsByTagName('h2')
+console.log (todosSubtitulos)
---
+++ clase-9/alcance.js
@@ -0,0 +1,20 @@
+ let num = 1 
+ console.log (num)
+ const saludar =()=>{
+    let num2=5
+    console.log (num*num2)
+ }
+ saludar()
+
+ const validarEdad = ()=>{
+    let edad =19 
+    if (edad>17){
+        let mensaje = "wakanda for ever"
+        console.log("es mayor de edad")
+        console.log(mensaje)
+    }else{
+        //console.log (mensaje)
+        console.log("estas sardino")
+    }
+ }
+ validarEdad()
